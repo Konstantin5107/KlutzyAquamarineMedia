20029bc311ae5876cea246b2d3f0b4c0fa88e02d
Konstantin5107 <kd06091950@gmail.com>
2025-04-23 07:18:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ number = random.randint(0, 5)
 print("Попробуй угадать число от 0 до 5!")
 user_number = int(input("Введи число: "))
 attempts = 1
-while user_number != number and attempts < 3:
-    user_number = ("Не угадал")
 while user_number != number and attempts <3:
     print("Не угадал")
     user_number -= user_number
@@ -13,6 +11,4 @@ while user_number != number and attempts <3:
 if user_number == number:
     print("Это верно, я загадал число", number)
     print("Ты выиграл")
-else:
-    print("Ты проиграл")elif attempts == 3 and user_number != number:
     print("Ты проиграл")
